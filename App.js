75f3016b4735f1b616b0f311d4713f563acdfbd4
Denwa <denwa799@yandex.ru>
2021-09-27 23:02:21 +0300

    Add output of elements

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 import {Navbar} from './src/components/Navbar'
 import {AddTodo} from "./src/components/AddTodo";
+import {Todo} from "./src/components/Todo";
 
 export default function App() {
-  return (
-    <View>
-      <Navbar title="Навигационная панель"/>
-      <View style={styles.container}>
-          <AddTodo/>
-      </View>
-    </View>
-  );
+    const [todos, setTodos] = useState([])
+
+    const addTodo = (title) => {
+        setTodos(prev => [
+            ...prev,
+            {
+                id: Date.now().toString(),
+                title
+            }])
+    }
+
+    return (
+        <View>
+            <Navbar title="Навигационная панель"/>
+            <View style={styles.container}>
+                <AddTodo onSubmit={addTodo}/>
+
+                <View>
+                    {todos.map(todo => (<Todo todo={todo} key={todo.id}/>
+                    ))}
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-      padding: 10
-  },
+    container: {
+        padding: 10
+    },
 });
