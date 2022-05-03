8c6065ed126fd94ea3b6d04a9f7daefba5f9cc73
Denwa <denwa799@yandex.ru>
2022-05-03 17:19:34 +0300

    Add loader

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -57,12 +57,14 @@ export const TodoState = ({children}) => {
     }
 
     const fetchTodos = async () => {
+        showLoader()
         const response = await fetch('https://react-native-todo-677d4-default-rtdb.europe-west1.firebasedatabase.app/todos.json', {
             headers: {'Content-Type': 'application/json'}
         })
         const data = await response.json()
         const todos = Object.keys(data).map(key => ({ ...data[key], id: key }))
         dispatch({type: FETCH_TODOS, todos})
+        hideLoader()
     }
 
     const updateTodo = (id, title) => dispatch({type: UPDATE_TODO, id, title})
